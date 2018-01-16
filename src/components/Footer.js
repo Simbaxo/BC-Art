f5293f0e2f7372db0210ebb4ed12b9d72f43a980
Andres <ballerlikemike23@gmail.com>
2018-01-15 23:47:41 -0500

    Full Images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
+                        <li><a href='https://twitter.com/B3NJA_92' className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+                        <li><a href='https://www.instagram.com/benja_yooo/' className="icon fa-instagram"><span className="label">Github</span></a></li>
+                        <li><a href="#" className="icon fa-facebook"><span className="label">Dribbble</span></a></li>
                         <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                     </ul>
                     <ul className="copyright">
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,20 @@ import avatar from '../assets/images/avatar.jpg'
 
 class Header extends React.Component {
     render() {
-        return (
-            <header id="header">
-                <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
-                </div>
-                <Footer />
-            </header>
-        )
+        return <header id="header">
+            <div className="inner">
+              <a href="#" className="image avatar">
+                <img src={avatar} alt="" />
+              </a>
+              <h1>
+                <strong>My name is Benja</strong>
+                <br />
+                Welcome To My Art Store<br />
+                crafted by <a href="http://html5up.net">Simba Knows</a>.
+              </h1>
+            </div>
+            <Footer />
+          </header>
     }
 }
 
